0c40ee195dd876a7705f37bbf71d53599e1210c3
mark.onderwater <mark.onderwater@cloudnation.nl>
2022-02-08 10:31:13 +0100

    add pipeline

--- src/lib/pipeline-stack.ts
+++ src/lib/pipeline-stack.ts
@@ -10,7 +10,7 @@ export class PipelineStack extends cdk.Stack {
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'MyPipeline',
             synth: new ShellStep('Synth', {
-                input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+                input: CodePipelineSource.gitHub('Mark-CloudNation/project1', 'master'),
                 commands: ['npm ci', 'npm run build', 'npx cdk synth']
             })
         });
